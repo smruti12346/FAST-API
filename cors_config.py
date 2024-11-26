14bf153e50f5cdc32da96c6bbecc030c99172e0a
PrabhuCharanTechnovate <prabhucharan.thetechnovate@gmail.com>
2024-11-26 11:30:58 +0530

    26-11-2024 commit -5

--- cors_config.py
+++ cors_config.py
@@ -4,12 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "https://riverranch-api.shoppingxperts.com",
-    "https://admin.riverranchdesigns.com",
-    "https://riverranchdesigns.com",
-    "https://www.riverranchdesigns.com",
-    "https://thera-posture.com",
-    "https://www.thera-posture.com",
 ]
 
 
--- db.py
+++ db.py
@@ -2,8 +2,4 @@ from pymongo import MongoClient
 # uri = "mongodb://technovate:pbJ76c9FKuSByMK1@ec2-52-2-7-108.compute-1.amazonaws.com:27017/"
 uri = "mongodb+srv://prabhucharanthetechnovate:pbJ76c9FKuSByMK1@cluster0.qagcixp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 my_client = MongoClient(uri)
-# db = my_client["zvu_ecom_db"]
-# db = my_client["riverranch_ecom_db"]
-# db = my_client["ecommerce_project"]
-db = my_client["thera_posture_ecom_db"]
-# db = my_client["thread_fusion_ecom_db"]
+db = my_client["ecommerce_project"]
